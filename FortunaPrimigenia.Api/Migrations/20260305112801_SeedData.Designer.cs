508ed834443e7fd6ebb62328faf817786f650d72
Theoxenos <theoxenos@pm.me>
2026-03-05 17:30:26 +0100

    Add new migration for more seed data

--- FortunaPrimigenia.Api/Migrations/20260305112801_SeedData.Designer.cs
+++ FortunaPrimigenia.Api/Migrations/20260305112801_SeedData.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FortunaPrimigenia.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FortunaPrimigenia.Api.Migrations
 {
     [DbContext(typeof(FortunaPrimigeniaContext))]
-    partial class FortunaPrimigeniaContextModelSnapshot : ModelSnapshot
+    [Migration("20260305112801_SeedData")]
+    partial class SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.3");
---
+++ FortunaPrimigenia.Api/Migrations/20260305112801_SeedData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FortunaPrimigenia.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Accounts",
+                columns: new[] { "Id", "Balance", "CreatedDate", "Currency", "IsOnBudget", "ModifiedDate", "Name", "Type" },
+                values: new object[] { 1, 1000m, new DateTime(2026, 3, 3, 8, 0, 0, 0, DateTimeKind.Utc), "EUR", false, new DateTime(2026, 3, 3, 8, 0, 0, 0, DateTimeKind.Utc), "ING Bank", "Checking" });
+
+            migrationBuilder.InsertData(
+                table: "Transactions",
+                columns: new[] { "Id", "AccountId", "CategoryId", "CreatedAt", "InflowAmount", "IsCleared", "Memo", "OutflowAmount", "Payee", "TransactionDate", "UpdatedAt" },
+                values: new object[] { -1, 1, 4, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 0m, false, "Dinner for coming week", 100m, "Supermarket", new DateTime(2026, 3, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Transactions",
+                keyColumn: "Id",
+                keyValue: -1);
+
+            migrationBuilder.DeleteData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
